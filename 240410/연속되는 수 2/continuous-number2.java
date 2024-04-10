139784c4ad600e51a2cea14ca7ce4efced814ae0
ScottSung7 <98432596+ScottSung7@users.noreply.github.com>
2024-04-10 13:18:58 +0900

    Title: 연속되는 수 2, Time: 187ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240410/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        
+        int max = 0;
+        int cnt = 0;
+        int numBefore = -1;
+        for(int i = 0; i < length; i++){
+            int a = sc.nextInt();
+            if(numBefore > 0 && a == numBefore){
+                cnt++;
+            }else{
+                cnt = 1;
+            }
+            numBefore = a;
+            if(cnt > max){
+                max = cnt;
+            }
+
+        }
+        System.out.println(max);
+
+
+
+    }
+}
