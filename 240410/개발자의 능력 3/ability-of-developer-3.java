0811da775c3294970522063c8ac451f85575317a
ScottSung7 <98432596+ScottSung7@users.noreply.github.com>
2024-04-10 19:46:06 +0900

    Title: 개발자의 능력 3, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240410/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 6;
+        int k = 3;
+        int[] arr = new int[n];
+        int allSum = 0;
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt();
+            arr[i] = a;
+            allSum += a;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i <= n-k; i++)
+            for(int j = i+1; j <= n-(k-1); j++)
+                for(int h = j+1; h<=n; h++){
+                    int sum1 = arr[i] + arr[j] + arr[k];
+                    int sum2 = allSum - sum1;
+                    int dif = Math.abs(sum1 - sum2);
+                    min = Math.min(min, dif);
+                }
+            
+        System.out.println(min);
+    }
+    
+}
