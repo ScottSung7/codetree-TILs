59c9d28e2eb038cee70420acb2d4594d9066301f
ScottSung7 <98432596+ScottSung7@users.noreply.github.com>
2024-04-14 09:50:11 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240414/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            int num = sc.nextInt();
+            int place = sc.nextInt();
+            if(place == 0){
+                place = 3;
+            }else if(place == 1){
+                place = 4;
+            }
+            if(arr[num] == 0){
+                arr[num] = place;
+            }else if(arr[num] != 0 && arr[num] != place){
+                cnt++;
+                arr[num] = place;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
