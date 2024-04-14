e01c38082ef9f14f0595cf80ec378e77bd423fc0
ScottSung7 <98432596+ScottSung7@users.noreply.github.com>
2024-04-14 09:51:32 +0900

    Title: 비둘기와 전기줄, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240414/비둘기와 전기줄/pigeons-and-electric-cords.java
+++ 240414/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[11];
         int cnt = 0;
         for(int i = 0; i < n; i++){
             int num = sc.nextInt();
